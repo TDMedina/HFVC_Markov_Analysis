f877ebedaad2f82dd0576d6db2b1f3bb24434cba
TDMedina <tylerdanmedina@gmail.com>
2022-03-02 22:55:51 +0000

    Added methods to shift date by 1 month or 1 year.

--- utilities.py
+++ utilities.py
@@ -1,6 +1,8 @@
 """HFVC - Utility Objects."""
 
 import pandas as pd
+from datetime import datetime
+
 
 class DatedValue:
     def __init__(self, name, value, date):
@@ -21,3 +23,13 @@ class DatedValue:
 class NamedDate(DatedValue):
     def __init__(self, name, date):
         super().__init__(name=name, value=pd.NA, date=date)
+
+
+def advance_month(date):
+    if date.month == 12:
+        return datetime(date.year + 1, 1, 1).date()
+    return datetime(date.year, date.month + 1, 1).date()
+
+
+def advance_year(date):
+    return datetime(date.year + 1, 1, 1).date()
