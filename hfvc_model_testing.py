1fae31265635a596b8d6d6dc82968b1082584ac7
TDMedina <tylerdanmedina@gmail.com>
2022-03-25 18:52:36 +0000

    Changed main testing methods to use new namedtuple.

--- hfvc_model_testing.py
+++ hfvc_model_testing.py
@@ -11,6 +11,8 @@ import hfvc_hmm
 
 EmitPrediction = namedtuple("EmitPrediction",
                             ["probabilities", "score", "probability_ratios"])
+PredictionTest = namedtuple("PredictionTest",
+                            ["correct", "truth", "predicted", "prediction"])
 
 def split_test_and_train(chains, random_test_state=False, proportion=10):
     n_chains = len(chains)
@@ -46,19 +48,29 @@ def test_hmm_model(test_chains, model):
     for chain, answer in test_chains:
         prediction = predict_next_emission(chain, *model)
         predicted_emit = np.argmax(prediction.probabilities)
-        results.append([predicted_emit == answer, predicted_emit, prediction])
+        results.append(PredictionTest(predicted_emit == answer, answer, predicted_emit, prediction))
     score = sum([result[0] for result in results]) / len(results)
     return score, results
 
 
 def build_and_test_model(data: hfvc.PatientDatabase, n_components, interval):
-    train, test = split_test_and_train(data.make_admission_chains(interval, True, 2))
-    init_params = hfvc_hmm.make_random_init_params(n_components, 3)
-    model = hfvc_hmm.multichain_baum_welch(train, **init_params)
+    train, test = split_test_and_train(data.make_admission_chains(interval, values_only=True, min_length=2))
+
+    # init_params = hfvc_hmm.make_random_init_params(n_components, 3)
+    # model = hfvc_hmm.multichain_baum_welch(train, **init_params)
+
+    tpm = hfvc_hmm.make_random_left_right_matrix(n_components)
+
+    # epm = hfvc_hmm.make_exclusive_deathstate_epm(n_components, 3)
+    epm = hfvc_hmm.make_uniform_stochastic_matrix(5,3)
+    # epm = np.array([[0.49, 0.49, 0.02]] * (n_components-1) + [[0.01, 0.01, 0.98]])
+
+    init_dist = hfvc_hmm.make_exclusive_deathstate_init_dist(n_components)
+    model = hfvc_hmm.multichain_baum_welch(train, tpm, epm, init_dist)
     score, results = test_hmm_model(test, model)
     return model, score, results, train, test
 
 
 if __name__ == "__main__":
     data = hfvc.main()
-    model, score, results, train, test = build_and_test_model(data, 5, "y")
+    model, score, results, train, test = build_and_test_model(data, 5, "m")
